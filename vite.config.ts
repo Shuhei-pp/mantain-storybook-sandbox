94d4bb2cfec245fde392ad3f9206826fea62484e
Shuhei-pp <lotakotayota@gmail.com>
2025-07-15 17:20:11 +0900

    init remix & storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,32 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vite.dev/config/
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
+
+const isStorybook = process.argv[1]?.includes("storybook");
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [!isStorybook && remix({
+    future: {
+      v3_fetcherPersist: true,
+      v3_relativeSplatPath: true,
+      v3_throwAbortReason: true,
+      v3_singleFetch: true,
+      v3_lazyRouteDiscovery: true
+    }
+  }), tsconfigPaths()],
   test: {
     projects: [{
       extends: true,
